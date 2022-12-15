dfe767b14b89129f7930363540070b3986c4c8a5
RaduSabau <sabau.radu91@yahoo.ro>
2022-12-15 15:51:42 +0200

    commit2

--- src/test/java/TestRunner.java
+++ src/test/java/TestRunner.java
@@ -5,7 +5,7 @@ import org.junit.runner.RunWith;
 @RunWith(Cucumber.class)
 @CucumberOptions(
         features = {"src/test/resources/FeatureFiles/"}
-        //tags = "@RegressionTestsBAP"
+        //tags = "@RegressionTestsBAP"git
 )
 public class TestRunner {
 }
