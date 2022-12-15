e39574a79bed5c90d742a94bcc3af702bb2e6021
Anto110 <antoniamoldovan13@gmail.com>
2022-12-15 20:47:04 +0200

    test1

---
+++ src/test/java/UITests/LoginSteps.java
@@ -0,0 +1,113 @@
+package UITests;
+
+import io.cucumber.java.en.*;
+
+public class LoginSteps {
+    @Given("I enter the username")
+    public void i_enter_the_username() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @When("I type <standard_ user\"> in \"name\"")
+    public void i_type_standard_user_in_name() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @When("I type password")
+    public void i_type_password() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @Then("I should be logged in")
+    public void i_should_be_logged_in() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @When("i click on refresh button")
+    public void i_click_on_refresh_button() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @Then("page should be refreshed")
+    public void page_should_be_refreshed() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @Given("I enter numbers on the username")
+    public void i_enter_numbers_on_the_username() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @Given("I let password as white space")
+    public void i_let_password_as_white_space() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @When("I press the login button")
+    public void i_press_the_login_button() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @Then("login shouldn't work")
+    public void login_shouldn_t_work() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @Given("I don't enter anything on username")
+    public void i_don_t_enter_anything_on_username() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @Given("i don't enter anything on password")
+    public void i_don_t_enter_anything_on_password() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @Then("an error pop-up is displayed")
+    public void an_error_pop_up_is_displayed() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @Given("I write wrong credentials")
+    public void i_write_wrong_credentials() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @Then("an error pop-up is displayed {string}")
+    public void an_error_pop_up_is_displayed(String string) {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @Given("valid username as standard user and blank password")
+    public void valid_username_as_standard_user_and_blank_password() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @When("I click on login")
+    public void i_click_on_login() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @Then("error message is displayed {string}")
+    public void error_message_is_displayed(String string) {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+}
