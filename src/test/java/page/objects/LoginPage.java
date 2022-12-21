a92c77594ebc0651aad9f86e6a692063944b4c32
dpopesco <diana.popesco17@gmail.com>
2022-12-21 19:19:53 +0200

    Am mutat elementele in LoginPageLocators si am scris metode get pentru ele

--- src/test/java/page/objects/LoginPage.java
+++ src/test/java/page/objects/LoginPage.java
@@ -2,10 +2,6 @@ package page.objects;
 
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.CacheLookup;
-import org.openqa.selenium.support.FindBy;
-import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
@@ -16,77 +12,55 @@ public class LoginPage {
 
     WebDriver driver;
     WebDriverWait wait;
+    LoginPageLocators loginLocators;
 
     public LoginPage(WebDriver driver) {
         this.driver = driver;
-        PageFactory.initElements(driver, this);
+        loginLocators = new LoginPageLocators(driver);
     }
 
-    @FindBy(id = LoginPageLocators.USERNAME_ID)
-    @CacheLookup
-    WebElement username;
-
-    @FindBy(id = LoginPageLocators.PASSWORD_ID)
-    @CacheLookup
-    WebElement password;
-
-    @FindBy(id = LoginPageLocators.LOGIN_ID)
-    @CacheLookup
-    WebElement loginButton;
-
-    @FindBy(xpath = LoginPageLocators.ERROR_XPATH)
-    @CacheLookup
-    WebElement errorMessage;
-
-    @FindBy(className = LoginPageLocators.HAMBURGER_BUTTON_CLS)
-    @CacheLookup
-    WebElement hamburgerButton;
-
-    @FindBy(xpath = LoginPageLocators.LOGOUT_BUTTON_XPATH)
-    @CacheLookup
-    WebElement logoutButton;
 
     public void setUsername(String uname) {
-        username.clear();
-        username.sendKeys(uname);
+        loginLocators.getUsername().clear();
+        loginLocators.getUsername().sendKeys(uname);
     }
 
     public void setPassword(String psw) {
-        password.clear();
-        password.sendKeys(psw);
+        loginLocators.getPassword().clear();
+        loginLocators.getPassword().sendKeys(psw);
     }
 
     public void clickLogin() {
-        loginButton.click();
+        loginLocators.getLoginButton().click();
     }
 
     public void checkHomePageOpened() {
         wait = new WebDriverWait(driver, Duration.ofSeconds(30));
-        wait.until(ExpectedConditions.visibilityOf(hamburgerButton));
-        Assert.assertTrue(hamburgerButton.isDisplayed());
+        wait.until(ExpectedConditions.visibilityOf(loginLocators.getHamburgerButton()));
+        Assert.assertTrue(loginLocators.getHamburgerButton().isDisplayed());
     }
 
     public void checkErrorMessage(String eMessage) {
-        Assert.assertEquals(errorMessage.getText(), eMessage);
+        Assert.assertEquals(loginLocators.getErrorMessage().getText(), eMessage);
     }
 
     public void enterKeyLogin() {
-        loginButton.sendKeys(Keys.ENTER);
+        loginLocators.getLoginButton().sendKeys(Keys.ENTER);
     }
 
     public void clickHamburgerBtn() {
-        hamburgerButton.click();
+        loginLocators.getHamburgerButton().click();
     }
 
     public void clickLogoutButton() {
-        wait.until(ExpectedConditions.visibilityOf(logoutButton));
-        logoutButton.click();
+        wait.until(ExpectedConditions.visibilityOf(loginLocators.getLogoutButton()));
+        loginLocators.getLogoutButton().click();
 
     }
 
     public void checkLoginPageOpened() {
-        PageFactory.initElements(driver, this);
-        wait.until(ExpectedConditions.visibilityOf(loginButton));
-        Assert.assertTrue(username.isDisplayed());
+        loginLocators.resetElement();
+        wait.until(ExpectedConditions.visibilityOf(loginLocators.getLoginButton()));
+        Assert.assertTrue(loginLocators.getUsername().isDisplayed());
     }
 }
