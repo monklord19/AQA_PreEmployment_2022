a76c5382d03f0cf88c867a4ed97a5a5601d54fba
Andreea <cazan.andreeaalina@gmail.com>
2023-01-03 15:58:03 +0200

    Cazan_Andreea_Bonus

--- src/test/java/UITests/StepsDef.java
+++ src/test/java/UITests/StepsDef.java
@@ -3,10 +3,13 @@ import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import org.junit.Assert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Wait;
 
 
 public class StepsDef {
@@ -25,6 +28,7 @@ public class StepsDef {
 
                     driver.findElement(By.id("user-name")).sendKeys(user);
                     driver.findElement(By.id("password")).sendKeys(pass);
+
                 }
 
                 @When("Click login button")
@@ -33,23 +37,43 @@ public class StepsDef {
                     driver.findElement(By.id("login-button")).click();
                 }
 
-                @Then("Login successful, home page displayed")
-                public void login_successful_home_page_displayed() throws InterruptedException {
-                   if ( driver.findElement(By.id("shopping_cart_container")).isDisplayed()) {
-                        driver.quit();
-                   }
-                    }
+//                @Then("Login successful, home page displayed")
+//                public void login_successful_home_page_displayed() throws InterruptedException {
+//                   if ( driver.findElement(By.id("shopping_cart_container")).isDisplayed()) {
+//                        driver.quit();
+//                   }
+//                    }
+//
+//    @Then("Error message displayed")
+//    public void errorMessageDisplayed() throws InterruptedException {
+//        if (driver.findElement(By.className("error-button")).isDisplayed()){
+//                        driver.quit();
+//                    }
+//    }
+//
+//    @And("Press enter key on keyboard")
+//    public void pressEnterKeyOnKeyboard() {
+//            driver.findElement(By.id("login-button")).sendKeys(Keys.ENTER);
+//
+//    }
 
-    @Then("Error message displayed")
-    public void errorMessageDisplayed() throws InterruptedException {
-        if (driver.findElement(By.className("error-button")).isDisplayed()){
-                        driver.quit();
-                    }
+    @When("I add the first item to Cart")
+    public void iAddTheFirstItemToCart() {
+                    driver.findElement(By.id("add-to-cart-sauce-labs-backpack")).click();
     }
 
-    @And("Press enter key on keyboard")
-    public void pressEnterKeyOnKeyboard() {
-            driver.findElement(By.id("login-button")).sendKeys(Keys.ENTER);
+    @And("I open my shopping cart")
+    public void iOpenMyShoppingCart() {
+                    driver.findElement(By.className("shopping_cart_badge")).click();
+    }
+
+    @Then("I should see the book in my shopping cart")
+    public void iShouldSeeTheBookInMyShoppingCart() {
+        WebElement element = driver.findElement(By.xpath("//*[@id=\"item_4_title_link\"]/div"));
+        String strng = element.getText();
+        System.out.println(strng);
+        Assert.assertEquals("Sauce Labs Backpack", strng);
+       // Assert.assertTrue(driver.findElement(By.className("inventory_item_name")),"Sauce Labs Backpack" );
     }
 }
 
--- src/test/resources/FeatureFilesAndreeaC/tema1.feature
+++ src/test/resources/FeatureFilesAndreeaC/tema1.feature
@@ -1,31 +1,38 @@
 Feature: Testing login page
 
-  Scenario Outline: Verify valid login
+#  Scenario Outline: Verify valid login
+#    Given I open login page
+#    When I enter <username> and <password>
+#    And Click login button
+#    Then Login successful, home page displayed
+#    Examples:
+#    | username | password |
+#    | standard_user|   secret_sauce  |
+#    | problem_user|  secret_sauce |
+#    | performance_glitch_user| secret_sauce |
+#
+#  Scenario Outline: Verify invalid login
+#    Given I open login page
+#    When I enter <username> and <password>
+#    And Click login button
+#    Then Error message displayed
+#    Examples:
+#      | username | password |
+#      | standard|sauce  |
+#      |         |       |
+#      |          |  secret_sauce |
+#      | performance_glitch_user|  |
+#
+#  Scenario: Verify if the ‘Enter’ key of the keyboard is working
+#    Given  I open login page
+#    When  I enter standard_user and secret_sauce
+#    And   Press enter key on keyboard
+#    Then   Login successful, home page displayed
+
+  Scenario: Consumer can add a book to shopping cart from wish list
     Given I open login page
-    When I enter <username> and <password>
+    And I enter standard_user and secret_sauce
     And Click login button
-    Then Login successful, home page displayed
-    Examples:
-    | username | password |
-    | standard_user|   secret_sauce  |
-    | problem_user|  secret_sauce |
-    | performance_glitch_user| secret_sauce |
-
-  Scenario Outline: Verify invalid login
-    Given I open login page
-    When I enter <username> and <password>
-    And Click login button
-    Then Error message displayed
-    Examples:
-      | username | password |
-      | standard|sauce  |
-      |         |       |
-      |          |  secret_sauce |
-      | performance_glitch_user|  |
-
-  Scenario: Verify if the ‘Enter’ key of the keyboard is working
-    Given  I open login page
-    When  I enter standard_user and secret_sauce
-    And   Press enter key on keyboard
-    Then   Login successful, home page displayed
-
+    When  I add the first item to Cart
+    And I open my shopping cart
+    Then I should see the book in my shopping cart
