ebe6471e3aeb56605016a23dda96c758888f7317
Stefania <lauran_denisa25@yahoo.com>
2023-01-03 11:58:14 +0200

    homework 2

--- src/test/java/MyStepdefs1.java
+++ src/test/java/MyStepdefs1.java
@@ -1,21 +1,50 @@
+import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class MyStepdefs1 {
+    WebDriver driver = new ChromeDriver();
     @Given("On sauce website")
     public void onSauceWebsite() {
-        WebDriver driver = new ChromeDriver();
+
         driver.get("https://www.saucedemo.com/");
     }
 
-    @When("User enters valid credentials")
-    public void userEntersValidCredentials() {
+    @When("user logging in using {string} and password as {string}")
+    public void userLoggingInUsingAndPasswordAs(String arg0, String arg1) {
+
+        driver.findElement(By.xpath("//*[@id='user-name']")).sendKeys(arg0);
+        driver.findElement(By.xpath("//*[@id='password']")).sendKeys(arg1);
     }
 
-    @Then("User should be logged in")
-    public void userShouldBeLoggedIn() {
+    @Then("user shouldn't be able to log in")
+    public void userShouldnTBeAbleToLogIn() {
+        driver.findElement(By.xpath("//*[@id='login-button']")).click();
+    }
+
+    @And("display {string}")
+    public void display(String arg0) {
+        driver.findElement(By.xpath("//*[@class='error-message-container error']")).sendKeys(arg0);
+
+    }
+
+
+    @When("User enters no credentials")
+    public void userEntersNoCredentials() {
+
+    }
+
+    @Then("User shouldn't be logged in")
+    public void userShouldnTBeLoggedIn() {
+        driver.findElement(By.xpath("//*[@id='login-button']")).click();
+    }
+
+    @And("display message {string}")
+    public void displayMessage(String arg0) {
+        driver.findElement(By.xpath("//*[@class='error-message-container error']")).sendKeys(arg0);
     }
 }
--- src/test/resources/FeatureFiles/feature.feature
+++ src/test/resources/FeatureFiles/feature.feature
@@ -2,5 +2,14 @@ Feature: Homework week2 marti
 
   Scenario:
     Given On sauce website
-    When User enters valid credentials
-    Then User should be logged in
+    When User enters no credentials
+    Then User shouldn't be logged in
+    And display message "Epic sadface: Username is required"
+
+  Scenario:
+    Given On sauce website
+    When user logging in using "random_user" and password as "secret_sauce"
+    Then user shouldn't be able to log in
+    And display "Epic sadface: Username and password do not match any user in this service"
+
+
