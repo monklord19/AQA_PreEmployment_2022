0aa3216c17cb337bfafdd7fcb8811ff5a03468a6
Gyorgy Istvan Gyorfi <istvang@fortech.ro>
2023-01-12 15:37:22 +0200

    clean-up main

--- src/test/java/TestRunner.java
+++ src/test/java/TestRunner.java
@@ -5,7 +5,7 @@ import org.junit.runner.RunWith;
 @RunWith(Cucumber.class)
 @CucumberOptions(
         features = {"src/test/resources/FeatureFiles/"}
-        //tags = "@RegressionTestsBAP"
+
 )
 public class TestRunner {
 }
