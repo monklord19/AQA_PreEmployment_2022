5dc6120235da9a8bec13a7e60993f1cc767af6c6
Adrian <cool.adryan@yahoo.com>
2023-01-25 22:36:19 +0200

    postman

---
+++ src/test/java/ApiTests/SecondApiTests.java
@@ -0,0 +1,67 @@
+package ApiTests;
+
+import io.restassured.RestAssured;
+import io.restassured.http.Header;
+import io.restassured.http.Headers;
+import io.restassured.response.Response;
+import io.restassured.specification.RequestSpecification;
+import org.junit.Test;
+
+import static io.restassured.RestAssured.given;
+
+public class SecondApiTests {
+    @Test
+    public void getAllUsers() {
+        String endpoint = "https://reqres.in/api/users?page=2";
+        given().when().get(endpoint).then().log().body();
+    }
+
+    @Test
+    public void getUserEight() {
+        RestAssured.baseURI = "https://reqres.in/api/users/8";
+        RequestSpecification httpRequest = given();
+        Response response = httpRequest.get("");
+        Headers allHeaders = response.headers();
+        for (Header header : allHeaders) {
+            System.out.println("Key:" + header.getName() + "Value" + header.getValue());
+        }
+    }
+
+    @Test
+    public void createUniqueIdUser() {
+        String endpoint = "https://reqres.in/api/users";
+        String body = """
+                {
+                "name": "morpheus",
+                "job": "leader"
+                }
+                """;
+        given().body(body).when().post(endpoint).then().log().body();
+    }
+
+    @Test
+    public void registerExistingUser() {
+        String endpoint = "https://reqres.in/api/register";
+        String body = """
+                {
+                 "email": "michael.lawson@reqres.in",
+                 "first_name": "Michael",
+                 "last_name": "Lawson",
+                 "avatar": "https://reqres.in/img/faces/7-image.jpg"
+                }
+                """;
+        given().body(body).when().post(endpoint).then().log().body();
+    }
+    @Test
+    public void updateUserEight() {
+        String endpoint = "https://reqres.in/api/users/8";
+        String body = """
+                {
+                "name": "morpheus test",
+                "job": "tester"
+                }
+                """;
+        given().body(body).when().post(endpoint).then().log().body();
+    }
+}
+
