4c3fe3df17533c9c81cf4b180d06ea66d189fd3d
Ivascu_Stefana_AQA-PRE_2022 <stefanaa29@yahoo.com>
2023-01-30 22:47:34 +0200

    API Tests using Feature File

--- src/test/java/APITests.java
+++ src/test/java/APITests.java
@@ -3,17 +3,19 @@ import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import io.restassured.response.Validatable;
 import io.restassured.response.ValidatableResponse;
+import org.hamcrest.Matchers;
 
 import static io.restassured.RestAssured.given;
 
 public class APITests {
     String BaseUrl;
     String endpoint;
+    String body;
     ValidatableResponse response;
-    @Given("user opens website {string}")
-    public void userOpensWebsiteHttpsReqresIn() {
-        BaseUrl =  "https://reqres.in/";
 
+    @Given("user opens website {string}")
+    public void userOpensWebsite(String link) {
+        BaseUrl = link;
     }
 
     @When("user wants to get list users")
@@ -24,18 +26,100 @@ public class APITests {
 
     @Then("the response should be success")
     public void theResponseShouldBeSuccess() {
+        response.log().body();
         response.assertThat().statusCode(200);
     }
 
-    @When("user wants to get  single list")
-    public void userWantsToGetSingleList() {
+    @When("user wants to get single list")
+    public void user_wants_to_get_single_list() {
+        endpoint = "api/users/2";
+        response = given().when().get(BaseUrl + endpoint).then();
     }
 
     @When("user wants to create a user")
     public void userWantsToCreateAUser() {
+        endpoint = "api/users";
+        body = """ 
+                {
+                     "name": "Matei",
+                     "job": "Bucatar sef"
+                 }
+                """;
+        response = given().body(body).when().post(BaseUrl + endpoint).then();
+    }
+    @Then("the response should be created")
+    public void theResponseShouldBeCreated() {
+        response.log().body();
+        response.assertThat().statusCode(201);
     }
 
-    @Then("the response should be {string}")
-    public void theResponseShouldBe(int arg0) {
+    @When("user wants to get single user not found")
+    public void userWantsToGetSingleUserNotFound() {
+        endpoint = "api/users/23";
+        response = given().when().get(BaseUrl + endpoint).then();
     }
+
+    @Then("the response should be not found")
+    public void theResponseShouldBeNotFound() {
+        response.log().body();
+        response.assertThat().statusCode(404);
+    }
+
+
+    @When("user wants to get resource list")
+    public void userWantsToGetResourceList() {
+        endpoint = "api/unknown";
+        response = given().when().get(BaseUrl + endpoint).then();
+    }
+
+    @When("user wants to get single resource")
+    public void userWantsToGetSingleResource() {
+        endpoint = "api/unknown/2";
+        response = given().when().get(BaseUrl + endpoint).then();
+    }
+
+    @When("user wants to get single resource not found")
+    public void userWantsToGetSingleResourceNotFound() {
+        endpoint = "api/unknown/23";
+        response = given().when().get(BaseUrl + endpoint).then();
+    }
+
+    @When("user wants to update a user")
+    public void userWantsToUpdateAUser() {
+        endpoint = "api/users/2";
+        body = """
+                {
+                    "name": "morpheus",
+                    "job": "zion resident"
+                }
+                """;
+        response = given().body(body).when().put(BaseUrl + endpoint).then();
+    }
+
+    @When("user wants to delete a user")
+    public void userWantsToDeleteAUser() {
+        endpoint = "api/users/2";
+        response = given().when().delete(BaseUrl + endpoint).then();
+
+    }
+
+    @Then("the response should be no content")
+    public void theResponseShouldBeNoContent() {
+        response.assertThat().statusCode(204);
+    }
+
+    @When("user wants to get delayed response")
+    public void userWantsToGetDelayedResponse() {
+        endpoint = "api/users";
+        response = given().queryParam("delay", "3").when().get(BaseUrl + endpoint).then();
+
+    }
+
+    @Then("the response should be success and greater than 3000L")
+    public void theResponseShouldBeSuccessAndTheTimeDisplayed() {
+        response.assertThat().statusCode(200);
+        response.time(Matchers.greaterThan(3000L));
+    }
+
+
 }
--- src/test/resources/FeatureFiles/FirstApiTests.feature
+++ src/test/resources/FeatureFiles/FirstApiTests.feature
@@ -1,16 +1,51 @@
 Feature:Testing a REST API
 
   Scenario: User wants to see list users
-    Given user opens website 'https://reqres.in/'
+    Given user opens website "https://reqres.in/"
     When user wants to get list users
     Then the response should be success
 
   Scenario: User wants to see single user
-    Given user opens website 'https://reqres.in/'
-    When user wants to get  single list
+    Given user opens website "https://reqres.in/"
+    When user wants to get single list
     Then the response should be success
 
   Scenario: User wants to create a user
-    Given user opens website 'https://reqres.in/'
+    Given user opens website "https://reqres.in/"
     When user wants to create a user
-    Then the response should be '201'
+    Then the response should be created
+
+  Scenario: User wants to get single user not found
+    Given user opens website "https://reqres.in/"
+    When user wants to get single user not found
+    Then the response should be not found
+
+  Scenario: User wants to see resource list
+    Given user opens website "https://reqres.in/"
+    When user wants to get resource list
+    Then the response should be success
+
+  Scenario: User wants to see  single resource
+    Given user opens website "https://reqres.in/"
+    When user wants to get single resource
+    Then the response should be success
+
+  Scenario: User wants to get single resource not found
+    Given user opens website "https://reqres.in/"
+    When user wants to get single resource not found
+    Then the response should be not found
+
+  Scenario: User wants to update a user
+    Given user opens website "https://reqres.in/"
+    When user wants to update a user
+    Then the response should be success
+
+  Scenario: User wants to delete a user
+    Given user opens website "https://reqres.in/"
+    When user wants to delete a user
+    Then the response should be no content
+
+  Scenario: User wants to get delayed response
+    Given user opens website "https://reqres.in/"
+    When user wants to get delayed response
+    Then the response should be success and greater than 3000L
