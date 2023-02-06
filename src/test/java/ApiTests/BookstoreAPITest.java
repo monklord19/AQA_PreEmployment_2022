491d75f368ec18d91eb926e757892f221e705a4a
Andrei Lapusan <andrei.lapusan97@yahoo.com>
2023-02-06 19:41:27 +0200

    Add more API Tests

--- src/test/java/ApiTests/BookstoreAPITest.java
+++ src/test/java/ApiTests/BookstoreAPITest.java
@@ -21,4 +21,49 @@ public class BookstoreAPITest {
                 .assertThat()
                 .statusCode(200);
     }
+    @Test
+    public void postBookStoreBooks() {
+        var body = """
+                    {
+                               "userId": "152343",
+                               "collectionOfIsbns": [
+                                 {
+                                   "isbn": "72568778"
+                                 }
+                               ]
+                             }
+                """;
+        var response = given()
+                .body(body)
+                .when()
+                .post(BookstoreAPIpage.postBookStoreBooks)
+                .then()
+                .log()
+                .body();
+
+        response
+                .assertThat()
+                .statusCode(201);
+    }
+    @Test
+    public void deleteBookStoreBooks() {
+        var body = """
+                {
+                  "isbn": "72568778",
+                  "userId": "152343"
+                }
+                """;
+
+        var response = given()
+                .body(body)
+                .when()
+                .delete(BookstoreAPIpage.deleteBookStoreBooks)
+                .then()
+                .log()
+                .body();
+
+        response
+                .assertThat()
+                .statusCode(204);
+    }
 }
