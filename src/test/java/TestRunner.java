b494689b2e80f446f5b046f43fd706983bfe97de
Anto110 <antoniamoldovan13@gmail.com>
2023-02-07 09:33:10 +0200

    Proiect final

--- src/test/java/TestRunner.java
+++ src/test/java/TestRunner.java
@@ -4,7 +4,8 @@ import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
-        features = {"src/test/resources/FeatureFiles/"}
+        features = {"src/test/resources/FeatureFiles/"},
+        glue = "MySteyDef"
 
 )
 public class TestRunner {
