b80f2732ae556cdd8f31e224757b6f3a55c30723
DalianaDriga <daliana.driga@fortech.ro>
2023-02-13 09:03:15 +0200

    Daliana Driga: BE scenarios added (Generate token & Authorize user)

--- src/test/java/StepDefinitions/SwaggerAccountStepDefs.java
+++ src/test/java/StepDefinitions/SwaggerAccountStepDefs.java
@@ -10,38 +10,65 @@ import org.junit.Assert;
 import org.junit.jupiter.api.Assertions;
 
 import static io.restassured.RestAssured.given;
+import static org.asynchttpclient.Dsl.request;
+import static org.hamcrest.CoreMatchers.equalTo;
 
 public class SwaggerAccountStepDefs {
 
     public static final String BASE_URL = "https://demoqa.com/Account/v1";
     JsonObject createRequest = new JsonObject();
-    private static Response response;
-    @Given("I am on the reqres website")
-    public void iAmOnTheReqresWebsite() {
+    static String username = "Daliana";
+    static String password = "Bla!123@";
+    private static Response responseCreateUser;
+    private static Response responseAuthorize;
+    private static Response responseGenerateToken;
+    @Given("I am on the BookStore website")
+    public void iAmOnTheBookStoreWebsite() {
         RestAssured.baseURI = BASE_URL + "/User";
         }
 
     @When("I a user with a specific username and password")
     public void iAUserWithASpecificUsernameAndPassword() {
-        createRequest.add("userName", "Daliana");
-        createRequest.add("password", "Bla!123@");
+        createRequest.add("userName", username);
+        createRequest.add("password", password);
         System.out.println(createRequest);
         System.out.println(createRequest.toString());
-        response = given().body(createRequest.toString()).when().post(RestAssured.baseURI).then().extract().response();
-        System.out.println("Response body: " + response.getBody().asString());
+        responseCreateUser = given().body(createRequest.toString()).when().post(RestAssured.baseURI).then().extract().response();
+        System.out.println("Response body: " + responseCreateUser.getBody().asString());
+        System.out.println("User ID is " + responseCreateUser.jsonPath().get("userID"));
     }
 
     @Then("The user is added")
     public void theUserIsAdded() {
-        int statusCode = response.getStatusCode();
+        int statusCode = responseCreateUser.getStatusCode();
         if (statusCode==200) {
             System.out.println("The user was successfully created");
-            Assert.assertNotNull(response.jsonPath().get("Daliana"));
-            Assert.assertEquals("Daliana", response.jsonPath().get("username"));
+            Assert.assertNotNull(responseCreateUser.jsonPath().get("userName"));
+            Assert.assertEquals("Daliana", responseCreateUser.jsonPath().get("username"));
         }
         if (statusCode==406) {
             System.out.println("The user already exists");
-            Assert.assertEquals("User exists!", response.jsonPath().get("message"));
+            Assert.assertEquals("User exists!", responseCreateUser.jsonPath().get("message"));
         }
+    }
+
+    @When("I authorize and generate token a user with a specific username and password")
+    public void iAuthorizeAndGenerateTokenAUserWithASpecificUsernameAndPassword() {
+        RestAssured.baseURI = BASE_URL;
+        JsonObject request = new JsonObject();
+        request.add("userName", username);
+        request.add("password", password);
+        responseAuthorize = given().header("Content-Type", "application/json").body(request.toString()).
+                post(RestAssured.baseURI = BASE_URL + "/Authorized");
+        responseGenerateToken = given().header("Content-Type", "application/json").body(request.toString()).
+                post(RestAssured.baseURI = BASE_URL + "/GenerateToken");
+        System.out.println("Token is " + responseGenerateToken.jsonPath().get("token"));
+    }
+
+    @Then("The user is authorized")
+    public void theUserIsAuthorized() {
+        int status = responseGenerateToken.then().extract().statusCode();
+        Assertions.assertEquals(200, status);
+        responseGenerateToken.then().body("status", equalTo("Success")).log().all();
     }
 }
